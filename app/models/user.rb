6510e7548bc4e5ee278ba3f91747001c335a1acb
WilliamMena <williammena1991@gmail.com>
2018-09-27 16:26:16 -0400

    Adjust the initialize method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_many :transactions
   has_many :stocks, through: :transactions
+
+  attr_accessor :name
+  attr_accessor :email
+  
+  def initialize(name, email)
+    @name = name
+    @email = email
+    @cash = 5000
+  end
 end
