c49a58911d8947bb9ccab916c2cbb2b3ecd506b9
WilliamMena <williammena1991@gmail.com>
2018-10-02 13:02:06 -0400

    Add validate_user function across whole application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def validate_user
+    if current_user.nil?
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  # before_action :validate_user
+  before_action :validate_user
 
   def home
     render html: '<div>Welcome Home</div>'.html_safe
