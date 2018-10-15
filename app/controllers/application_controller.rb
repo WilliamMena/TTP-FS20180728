478b119fd1ed875f2ef863a8d54c37e0ddc479af
WilliamMena <williammena1991@gmail.com>
2018-10-15 14:09:11 -0400

    Devise now accepts :name for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     if current_user.nil?
       redirect_to root_path
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
